601abb25db55d1253c32eda31be6a0e88e33f0aa
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-10-26 18:24:05 +0530

    Create Average of numbers in an array.java

---
+++ Average of numbers in an array.java
@@ -0,0 +1,25 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+ 	     double[] arr = {12,23.8,16.5,200,13.7};
+ 	     double total = 0;
+ 	     for(int i = 0;i < arr.length;i++){
+ 	         total = total + arr[i];
+ 	     }
+	     double average = total /arr.length;
+	     System.out.format("The average is : %.3f",average);
+	}
+}
+public class Main
+{
+	public static void main(String[] args) {
+ 	     double[] arr = {12,23.8,16.5,200,13.7};
+ 	     double total = 0;
+ 	     for(int i = 0;i < arr.length;i++){
+ 	         total = total + arr[i];
+ 	     }
+	     double average = total /arr.length;
+	     System.out.format("The average is : %.3f",average);
+	}
+}
